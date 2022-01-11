9b8597cb80ac432d4e5f73c01261069f839bb192
Veeru-5462 <94423137+Veeru-5462@users.noreply.github.com>
2022-01-11 15:59:18 -0500

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -3,9 +3,9 @@ def call(credId,tomcatIp,tomcatUser)
   sshagent(['credId']) {
                     // some block
                     sh """
-                    scp -o StrictHostKeyChecking=no target/myweb*.war ${tomcatUser}@{tomcatIp}:/opt/tomcat8/webapps/myweb.war
-                    ssh  ${tomcatUser}@{tomcatIp} /opt/tomcat8/bin/shutdown.sh
-                    ssh  ${tomcatUser}@{tomcatIp} /opt/tomcat8/bin/startup.sh
+                    scp -o StrictHostKeyChecking=no target/myweb*.war ${tomcatUser}@${tomcatIp}:/opt/tomcat8/webapps/myweb.war
+                    ssh  ${tomcatUser}@${tomcatIp} /opt/tomcat8/bin/shutdown.sh
+                    ssh  ${tomcatUser}@${tomcatIp} /opt/tomcat8/bin/startup.sh
                     
                     """
                 }
