533a239a57743d7528fdc52164956ea28806bf79
Veeru-5462 <94423137+Veeru-5462@users.noreply.github.com>
2022-01-11 15:38:12 -0500

    Create tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,12 @@
+def call(CredId,tomcatIp,tomcatUser)
+{
+  sshagent(['Tomcat-dev']) {
+                    // some block
+                    sh """
+                    scp -o StrictHostKeyChecking=no target/myweb*.war ${tomcatUser}@{tomcatIp}:/opt/tomcat8/webapps/myweb.war
+                    ssh  ${tomcatUser}@{tomcatIp} /opt/tomcat8/bin/shutdown.sh
+                    ssh  ${tomcatUser}@{tomcatIp} /opt/tomcat8/bin/startup.sh
+                 
+                    """
+                }
+}
