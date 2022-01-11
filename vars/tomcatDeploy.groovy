0da910aeb8dc0af95fcf2758d381ed41535d2a9e
Veeru-5462 <94423137+Veeru-5462@users.noreply.github.com>
2022-01-11 15:47:31 -0500

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -6,7 +6,7 @@ def call(CredId,tomcatIp,tomcatUser)
                     scp -o StrictHostKeyChecking=no target/myweb*.war ${tomcatUser}@{tomcatIp}:/opt/tomcat8/webapps/myweb.war
                     ssh  ${tomcatUser}@{tomcatIp} /opt/tomcat8/bin/shutdown.sh
                     ssh  ${tomcatUser}@{tomcatIp} /opt/tomcat8/bin/startup.sh
-                 
+                    
                     """
                 }
 }
